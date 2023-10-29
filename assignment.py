4ec99a34b28fe8380df3ec618abe9d1ed5ab0676
root <v.bagherianno@gmail.com>
2023-10-29 13:00:46 +0000

    First Functional version

--- assignment.py
+++ assignment.py
@@ -14,6 +14,8 @@ d_th = 0.4
 R = Robot()
 """ instance of the class Robot"""
 
+GrabbedGold = list()
+
 def drive(speed, seconds):
     """
     Function for setting a linear velocity
@@ -45,9 +47,31 @@ def turn(speed, seconds):
 def Gold_find():
 
 	dist =100
+	
+	
 	for Box in R.see():
 	
-		if Box.dist<dist and Box.info.marker_type == MARKER_TOKEN_GOLD:
+		if Box.dist<dist and Box.info.marker_type == MARKER_TOKEN_GOLD and Box.info.code not in GrabbedGold:
+		
+			dist = Box.dist
+			rot_y = Box.rot_y
+			Code = Box.info.code
+			
+	if dist == 100:
+	
+		return -1 , -1 ,-1
+	
+	else:
+		return dist, rot_y ,Code
+		
+def Release_Loc_Find():
+
+	dist =100
+	
+	
+	for Box in R.see():
+	
+		if Box.dist<dist and Box.info.marker_type == MARKER_TOKEN_GOLD and Box.info.code in GrabbedGold:
 		
 			dist = Box.dist
 			rot_y = Box.rot_y
@@ -63,13 +87,14 @@ def Gold_find():
 
 def Gold_grab():
 
-				
-	while 1:
+	a = 1			
+	while a:
 	    dist, rot_y ,Code= Gold_find()  # we look for markers
 	    
 	    if dist <d_th: 
 		print("Found a Gold one!")	 
-		break
+		
+		a = 0
 	    elif -a_th<= rot_y <= a_th: # if the robot is well aligned with the token, we go forward
 		print("Going forward!.")
 		drive(10, 0.5)
@@ -80,41 +105,74 @@ def Gold_grab():
 		print("Right a bit...")
 		turn(+2, 0.5)
 
+def Release_Grabbed_Gold():
 
-Index = 1
-a = 1
+	a = 1			
+	while a:
+	    dist, rot_y ,Code= Release_Loc_Find()  # we look for markers
+	    
+	    if dist <d_th+0.2: 
+		print("Found a drop location!")	 
+		
+		a = 0
+	    elif -a_th<= rot_y <= a_th: # if the robot is well aligned with the token, we go forward
+		print("Going forward!.")
+		drive(10, 0.5)
+	    elif rot_y < -a_th: # if the robot is not well aligned with the token, we move it on the left or on the right
+		print("Left a bit...")
+		turn(-2, 0.5)
+	    elif rot_y > a_th:
+		print("Right a bit...")
+		turn(+2, 0.5)
+			
+		
+			
 
+	
 def main():
 
-	while a:
+	dist,rot_y,Code= Gold_find()
+	while dist == -1:
+		print("I have to search more for a gold box!!")
+		turn(5,2)
+		dist , rot_y , Code = Gold_find()
+	Gold_grab()
+	R.grab()
+	print("Just grabbed it")
+	drive(10 , 2)
+	R.release()
+	print("Package Delivered")
+	drive(-10 , 2)
+	turn(30,2)
+	GrabbedGold.append(Code)
+	
+	while len(GrabbedGold)< 6:
 			
-		dist,rot_y = Gold_find()
+		dist,rot_y,Code= Gold_find()
 		while dist == -1:
 			print("I have to search more for a gold box!!")
 			turn(5,2)
-			dist , rot_y = Gold_find()
+			dist , rot_y , Code = Gold_find()
 		Gold_grab()
 		R.grab()
-		dist,rot_y = Gold_find()
-		while dist == -1:
+		print("Just grabbed it")
+		Newdist,Newrot_y,NewCode = Release_Loc_Find()
+		
+		while Newdist == -1:
 			print("I have to search more for a destination!!")
 			turn(5,2)
-			dist , rot_y = Gold_find()
-		Gold_grab()
-		R.release()	
-		print("I need a new box to carry")
-		turn(-30,2)
+			Newdist , Newrot_y , NewCode = Release_Loc_Find()
+		Release_Grabbed_Gold()
+		R.release()
+		print("Package Delivered")
+		drive(-10,2)
+		turn(30,2)
+		GrabbedGold.append(Code)
 		
-dist,rot_y ,Code= Gold_find()
-print(dist,rot_y , Code)
-while dist == -1:
-	print("I have to search more for a gold box!!")
-	turn(5,2)
-	dist , rot_y , Code= Gold_find()
-Gold_grab()
-R.grab()
-dist,rot_y ,Code= Gold_find()
-print(dist,rot_y , Code)
+main()
+		
+		
+		
 	
 	
 	
